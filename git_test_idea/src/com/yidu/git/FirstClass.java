98efd8a5dc3dd96448a23d530537442326f4b833
wh <1075748804@qq.com>
2020-08-13 15:25:48 +0800

    idea把本地文件添加到Git

---
+++ git_test_idea/src/com/yidu/git/FirstClass.java
@@ -0,0 +1,17 @@
+package com.yidu.git;
+
+/**
+ * 类的描述:
+ *
+ * @author wh
+ * @since 2020/8/13 15:22
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("sdf");
+        System.out.println("sdf");
+        System.out.println("sdf");
+        System.out.println("sdf");
+        System.out.println("sdf");
+    }
+}
